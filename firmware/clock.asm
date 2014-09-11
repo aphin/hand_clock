08e4fb39eea394b6347ea4887c3f30746c53590c
aphin <vivalzard@mail.ru>
2014-09-11 22:55:17 +0400

    UPD 11.09.2014

--- firmware/clock.asm
+++ firmware/clock.asm
@@ -1,21 +1,6 @@
 ; Фирмварь для наручных часов на ATMEGA48PA, дисплей Siemens C60, 6 кнопок
 ; TODO
-; 1. Сделать более наглядную настройку времени (с подсветкой редактируемой величины)
-; 2. Сделать регистр флагов обновления, чтобы не выполнять лишние рисования
-
-
-;#define DEBUG
-
-; Для себя:
-; Регистры GPIORx
-; GPIOR0.0 - флаг переноса BCD
-; GPIOR0.1 - используется как вспомогательный флаг при опросе клавиатуры
-; GPIOR0.2 - устанавливается в 1 каждую десятую долю секунды
-; GPIOR0.3 - ...каждую секунду
-; GPIOR0.4 - ...минуту
-; GPIOR0.5 - ...час
-; GPIOR0.6 - ...день
-; GPIOR0.7 - ...месяц
+; 1. Запилить секундомер
 
 .nolist
 .include "C:\Program Files\Atmel\AVR Tools\AvrAssembler2\Appnotes\m48PAdef.inc"
@@ -73,10 +58,78 @@ wtr:
 	rjmp wtr
 
 ; обработка звука
-	tst tmp2
-	brne m_cont
+	tst snd_flag	; если звук выключен - не обрабатываем
+	breq m_tdr
+	
+	tst tmp2		; время вышло
+	brne m_snd_cont
+	ldi snd_flag, 0	; выключаем звук
+	ldi snd, 0
 	BEEP_OFF
-m_cont:
+	rjmp m_tdr
+
+m_snd_cont:
+	tst snd				; если счетчик не равен нулю, то
+	brne m_snd_cont
+	LAPZ sound_pattern	; загружаем следующий паттерн (или первый)
+	push snd_flag
+	dec snd_flag
+	add ZL, snd_flag
+	ldi tmp, 0x00
+	adc ZH, tmp
+	lpm snd, Z
+	pop snd_flag
+	sbrs snd_flag, 0	; проверяем на не-четность (так как флаг считается от 1-цы)
+	rjmp m_snd_silent	; если четный - то выключаем бипер
+	BEEP_ON				; если не четный - то включаем
+	rjmp m_snd_cont3
+m_snd_silent:
+	BEEP_OFF
+m_snd_cont3:
+	inc snd_flag
+	cpi snd_flag, 6
+	brne m_tdr
+	ldi snd_flag, 1
+
+; обработка таймера
+m_tdr:
+	sbrs refresh, 6 ; таймер запущен
+	rjmp m_cont
+	sbis GPIOR0, 3
+	rjmp m_cont
+	LASZ timer_sec
+	ld tmp, Z+
+	cpi tmp, 0x00
+	brne m_tdr1
+	ld tmp, Z
+	cpi tmp, 0x00
+	breq m_tdr_stop
+m_tdr1:
+	sbiw ZL, 0x01
+	ldi tmp, 0x99
+	rcall BCD_Add	; BCD_Dec
+	ld tmp, Z
+	cpi tmp, 0x60
+	brlo m_tdr_ref
+	ldi tmp, 0x59
+	st Z+, tmp
+	ldi tmp, 0x99
+	rcall BCD_Add	; BCD_Dec
+	ld tmp, Z
+m_tdr_ref:
+	ori refresh, 0b00100000
+	rjmp m_cont
+m_tdr_stop:
+	ldi tmp, 0x00
+	st Z, tmp
+	andi refresh, 0b10111111
+	ori refresh, 0b00100000
+	ldi snd_flag, 1	; звуковой сигнал
+	ldi snd, 0
+	ldi tmp2, 255
+
+
+m_cont:					; обработка режимов
 	cpi mode, 0x00
 	brne m_mode1
 
@@ -84,32 +137,35 @@ m_cont:
 	sbis GPIOR0, 3	; Выполняем рисование каждую секунду
 	rjmp kbd_hand
 
+m_draw_dt:
 	ldi tmp, 3
 	mov lcd_x, tmp
-	ldi tmp, 28
+	ldi tmp, 20
 	mov lcd_y, tmp
 	rcall LCD_DrawTime
 	rcall LCD_DrawDate
+	rcall LCD_DrawBattery
+	ldi tmp, 3
+	mov lcd_x, tmp
+	ldi tmp, 20
+	mov lcd_y, tmp
+
 	rjmp kbd_hand
+
 ; Режим 1 - рисуем дату-время, подсветка включена, режим Normal, рисуем с интервалом 0.1 сек
 m_mode1:
 	cpi mode, 0x01
 	brne m_mode2
 	sbis GPIOR0, 2
 	rjmp kbd_hand
-	
-;	ldi refresh, 0b00011111
+	rjmp m_draw_dt
 
-	ldi tmp, 3
-	mov lcd_x, tmp
-	ldi tmp, 28
-	mov lcd_y, tmp
-
-	rcall LCD_DrawTime
-	rcall LCD_DrawDate
-	
-	rjmp kbd_hand
 m_mode2:				
+; Режим 2 - здесь обрабатывается таймер
+	sbrs refresh, 5	; бит 5 указывает на необходимость обновления таймера
+	rjmp kbd_hand
+	andi refresh, 0b11011111
+	rcall LCD_DrawTimer
 
 kbd_hand:
 ; звук при нажатии на кнопки
@@ -117,8 +173,10 @@ kbd_hand:
 	ori tmp, 0b11000000
 	cpi tmp, 0xFF
 	breq kbd_hcont
-	BEEP_ON
-	ldi tmp2, 5
+	ldi snd_flag, 1 ; устанавливаем номер сэмпла (0) и включаем звук
+	ldi snd, 0
+	ldi tmp2, 20	; длительность задается так же как и для функций Delay (проиграется только бип)
+
 kbd_hcont:
 	cpi mode, 0x00
 	brne m_kbd_mode1
@@ -127,7 +185,7 @@ kbd_hcont:
 	rjmp nx_b
 	LED_ON
 	rcall LCD_IdleOff
-	ldi tmp, 50
+	ldi tmp, 40
 	mov led_timer, tmp
 nx_b:
 	sbrc kbd_press, 5
@@ -138,6 +196,41 @@ nx_b:
 	ori refresh, 0b00011111
 	LED_ON
 nx_b1:
+	sbrc kbd_press, 1	; включаем режим таймера
+	rjmp nx_b2
+	ldi mode, 0x02
+	rcall LCD_Clear
+	ldi tmp, 26
+	mov lcd_x, tmp
+	ldi tmp, 5
+	mov lcd_y, tmp
+	ldi tmp, 1
+	mov lcd_mx, tmp
+	mov lcd_my, tmp
+	LAPZ tim_str
+	rcall LCD_DrawStringPM
+	ori refresh, 0b00100000
+nx_b2:
+	sbrc kbd_press, 0	; изменяем режим отображения
+	rjmp nx_b3
+	inc style
+	cpi style, 3
+	brne nx_b2_c
+	ldi style, 0
+nx_b2_c:
+	ori refresh, 0b00000011
+	
+	ldi tmp, 20+14	; Очищаем область рисования секунд
+	mov lcd_y, tmp
+	ldi tmp, 3+(5*16)
+	mov lcd_x, tmp
+	ldi tmp, 2
+	mov lcd_mx, tmp
+	mov lcd_my, tmp
+	ldi tmp, 0x20
+	rcall LCD_DrawChar
+
+nx_b3:
 	rjmp main
 m_kbd_mode1:
 ; Обработчик кнопок режима 1
@@ -226,8 +319,7 @@ sec_corr:
 	sbiw ZL, 0x01
 	st Z, tmp	; обнуляем чило десятых долей секунд
 	rcall Tmp_to_TCNT2	; сброс счетчика таймера
-	LASZ sec_cnt
-	st Z, tmp	; обнуляем счетчик интервала 0.1 сек
+	clr sec_part_cnt ; обнуляем счетчик интервала 0.1 сек
 	rjmp no_corr
 day_corr:
 ; в стеке лежит скорректированное текущее число	
@@ -262,14 +354,56 @@ nx_b3_m1:
 	rjmp main
 
 m_kbd_mode2:
-; Обработчик кнопок режима 2
-	sbrc kbd_press, 5
+; Обработчик кнопок режима 2 (таймер)
+	sbrc kbd_press, 1
 	rjmp nx_b_m2
 	clr mode
 	rcall LCD_Clear
 	rcall LCD_IdleOn
-	LED_OFF
+	ori refresh, 0b00011111
 nx_b_m2:
+	sbrc kbd_press, 3
+	rjmp nx_b1_m2
+	LED_ON
+	rcall LCD_IdleOff
+	ldi tmp, 40
+	mov led_timer, tmp
+nx_b1_m2:
+	sbrc kbd_press, 0 ; старт/стоп таймера
+	rjmp nx_b2_m2
+	ldi tmp, 0b01000000
+	eor refresh, tmp
+nx_b2_m2:
+	sbrc kbd_press, 4 ; инкремент минут (только если таймер остановлен)
+	rjmp nx_b3_m2
+	sbrc refresh, 6
+	rjmp main
+	LASZ timer_min
+	rcall BCD_Inc
+	ori refresh, 0b00100000
+nx_b3_m2:
+	sbrc kbd_press, 5 ; декремент минут (тоже только если таймер остановлен)
+	rjmp nx_b4_m2
+	sbrc refresh, 6
+	rjmp main
+	LASZ timer_min
+	ldi tmp, 0x99
+	rcall BCD_Add
+	ori refresh, 0b00100000
+nx_b4_m2:
+	sbrc kbd_press, 2 ; инкремент секунд (так же если таймер остановлен)
+	rjmp main
+	sbrc refresh, 6
+	rjmp main
+	LASZ timer_sec
+	rcall BCD_Inc
+	ld tmp, Z
+	cpi tmp, 0x60
+	brne b4_m2_end
+	ldi tmp, 0x00
+	st Z, tmp
+b4_m2_end:
+	ori refresh, 0b00100000
 	rjmp main
 
 ; для экономии flash расчет смещения для mode_tmp вынесен в отдельную подпрограмму
@@ -323,9 +457,12 @@ T2_COMPA:		; обработчик прерывания от таймера
 	push tmp
 	in tmp, SREG
 	push tmp
+	push tmp1
 
 	push ZH
 	push ZL
+	push YH
+	push YL
 
 	in tmp, GPIOR0
 	andi tmp, 0b00000011
@@ -333,19 +470,57 @@ T2_COMPA:		; обработчик прерывания от таймера
 
 	rcall KBD_Check ; Скан кнопок
 	
-	ldi ZH, high(sec_duration)
-	ldi ZL, low(sec_duration)
-	ld tmp, Z
+	ldi tmp, TimCntVal
 	inc sec_part_cnt
 	cp sec_part_cnt, tmp
 	brne t2_end
 
-; Прошла 0.1 секунды
+; Прошла 1/8 секунды
 	sbi GPIOR0, 2	; Установили флаг
 	
 	rcall Time_Date_Inc	; Инкремент даты-времени
+t2_snd:
+	tst snd
+	breq t2_snd1
+	dec snd
+t2_snd1:
 	clr sec_part_cnt	; Очистили счетчик
+; Корректировка времени (ЦНХ)
+; Сравниваем текущее время с данными ЦНХ
+	LAPZ w_time_corr
+	ldi YH, high(dsecond)
+	ldi YL, low(dsecond)
+	lpm tmp, Z+
+	ld tmp1, Y+
+	cp tmp, tmp1	; сравниваем доли секунд
+	brne t2_no_tcor
+	lpm tmp, Z+
+	ld tmp1, Y+
+	cp tmp, tmp1	; сравниваем секунды	
+	brne t2_no_tcor
+	lpm tmp, Z+
+	ld tmp1, Y+
+	cp tmp, tmp1	; сравниваем минуты
+	brne t2_no_tcor
+	lpm tmp, Z
+	ld tmp1, Y
+	cp tmp, tmp1	; сравниваем часы
+	brne t2_no_tcor
+; Сюда попадаем только если все сравнения дали результат "равно", и коррекция еще не делалась (бит 7 регистра refresh = 0) - это значит надо делать коррекцию
+	sbrc refresh, 7
+	rjmp t2_no_tcor_r
+	sbiw YL, 3
+	ldi tmp, 0x00	; Устанавливаем время 00:00:00.0
+	st Y+, tmp
+	st Y+, tmp
+	st Y+, tmp
+	st Y, tmp
+	ori refresh, 0b10000000
+	rjmp t2_no_tcor
+t2_no_tcor_r:
+	andi refresh, 0b01111111
 ; Определяем, в каком режиме работают часы
+t2_no_tcor:
 	cpi mode, 0x00
 	brne tc2_mode1
 ; Режим 0 - управление подсветкой
@@ -359,16 +534,30 @@ T2_COMPA:		; обработчик прерывания от таймера
 
 tc2_mode1:
 ; Режим 1 - настройка даты, времени, корректировок
-	nop
+	cpi mode, 0x01
+	brne tc2_mode2
+
+	rjmp t2_end
+
+tc2_mode2:
+; Режим 2 - таймер
+	tst led_timer	; подсветка
+	breq t2_end
+	dec led_timer
+	brne t2_end
+	LED_OFF
 
 t2_end:
 	tst tmp2
 	breq t2_out1
 	dec tmp2
 t2_out1:
+	pop YL
+	pop YH
 	pop ZL
 	pop ZH
-t2_out:
+
+	pop tmp1
 	pop tmp
 	out SREG, tmp
 	pop tmp
@@ -430,6 +619,80 @@ kbd_ret:
 	pop tmp
 	ret
 ;----------------------
+LCD_DrawTimer:
+	push ZH
+	push ZL
+	push tmp
+	push tmp1
+
+	ldi tmp, 0x02
+	mov lcd_mx, tmp
+	ldi tmp, 0x02
+	mov lcd_my, tmp
+	ldi tmp, 10
+	mov lcd_x, tmp
+	ldi tmp, 20
+	mov lcd_y, tmp
+	ldi tmp1, 16
+	
+	LASZ timer_min
+	ld tmp, Z
+	rcall LCD_DrawBCD7
+	push lcd_my
+	ldi tmp, 0x04
+	mov lcd_my, tmp
+	ldi tmp, ':'
+	rcall LCD_DrawChar
+	pop lcd_my
+	add lcd_x, tmp1
+	sbiw ZL, 0x01
+	ld tmp, Z
+	rcall LCD_DrawBCD7
+
+	pop tmp1
+	pop tmp
+	pop ZL
+	pop ZH
+	ret
+;----------------------
+LCD_DrawBattery:
+	ldi tmp, 84
+	mov lcd_x, tmp
+	ldi tmp, 68
+	mov lcd_y, tmp
+	ldi tmp, 0x01
+	mov lcd_mx, tmp
+	mov lcd_my, tmp
+	ldi tmp, 0b11000001
+	sts ADCSRA, tmp
+vbat_mw:
+	lds tmp, ADCSRA
+	sbrs tmp, ADIF
+	rjmp vbat_mw
+	ldi tmp, 0b10010100
+	sts ADCSRA, tmp
+	lds tmp, ADCH
+	
+	cpi tmp, 0x4B
+	brsh vbat_nx1
+	ldi tmp, 0x2E	; Full
+	rjmp vbat_ex
+vbat_nx1:
+	cpi tmp, 0x50
+	brsh vbat_nx2
+	ldi tmp, 0x2D	; 60%
+	rjmp vbat_ex
+vbat_nx2:
+	cpi tmp, 0x5A
+	brsh vbat_nx3
+	ldi tmp, 0x2C	; 30%
+	rjmp vbat_ex
+vbat_nx3:
+	ldi tmp, 0x2B
+vbat_ex:
+	rcall LCD_DrawChar
+	ret
+;----------------------
 LCD_DrawTime:
 	push ZH
 	push ZL
@@ -438,7 +701,13 @@ LCD_DrawTime:
 
 	ldi tmp, 2
 	mov lcd_mx, tmp
-	ldi tmp, 3
+	cpi style, 1
+	brne drt1
+	ldi tmp, 2
+	rjmp drt2
+drt1:
+	ldi tmp, 4
+drt2:
 	mov lcd_my, tmp
 	ldi tmp1, 0xFF
 
@@ -456,11 +725,21 @@ LCD_DrawTime:
 	brne drw_clk_hrd
 	rcall Exch_bkg_frg
 drw_clk_hrd:
+	cpi style, 1
+	brne drt3
+	rcall LCD_DrawBCD7
+	rjmp drt4
+drt3:
 	rcall LCD_DrawBCD
+drt4:
 	pop frg_color
 	pop bkg_color
+	sbrc style, 0
+	lsl lcd_my
 	ldi tmp,':'
 	rcall LCD_DrawChar
+	sbrc style, 0
+	lsr lcd_my
 	rjmp drw_clk_minute
 drw_clk_minute_skip:
 	ldi tmp, 32
@@ -481,7 +760,13 @@ drw_clk_minute:
 	brne drw_clk_mnd
 	rcall Exch_bkg_frg
 drw_clk_mnd:
+	cpi style, 1
+	brne drt5
+	rcall LCD_DrawBCD7
+	rjmp drt6
+drt5:
 	rcall LCD_DrawBCD
+drt6:
 	pop frg_color
 	pop bkg_color
 	rjmp drw_clk_sec
@@ -505,7 +790,13 @@ drw_clk_sec:	; рисуем секунды
 	brne drw_clk_sed
 	rcall Exch_bkg_frg
 drw_clk_sed:
+	cpi style, 1
+	brne drt7
+	rcall LCD_DrawBCD7
+	rjmp drt8
+drt7:
 	rcall LCD_DrawBCD
+drt8:
 	pop frg_color
 	pop bkg_color
 
@@ -528,6 +819,24 @@ Time_Date_Inc:
 	breq tdi_cnt1
 	rjmp tdi_end
 tdi_cnt1:
+
+; делаем задержку на 1 такт таймера TC2 (поправка, т.к. по всем "прикидкам" система считает 32767 тактов в секунду вместо 32768)
+; Не энергоэффективно, т.к. в этом куске кода процессор "тупо" ждет пока таймер отсчитает 3 такта
+; можно ускорить процесс исключив ожидание вначале появления в регистре счетчика 0х01, и записывая при этом в регистр таймера уже значение 0х01 вместо
+; 0х02
+
+tdi_fix_w:
+	lds tmp, TCNT2
+	cpi tmp, 0x01
+	brlo tdi_fix_w
+	ldi tmp, 0x02	; записываем в счетчик 0х02, (выполнение кода-поправки займет 3 такта таймера, мы же записали сюда 2, таким образом сделав задержку на такт)
+	sts TCNT2, tmp
+tdi_fix_w1:
+	lds tmp, ASSR
+	sbrc tmp, TCN2UB
+	rjmp tdi_fix_w1
+; ---
+
 	clr tmp
 	sbi GPIOR0, 3
 	st Z+, tmp		
@@ -629,10 +938,12 @@ BCD_Add:
 	
 	push tmp1
 	push tmp
+	push r20
 
 	cbi GPIOR0, 0
 	ld tmp1, Z
 	add tmp1, tmp
+	in r20, SREG
 	brhs lsd_correct
 	push tmp1
 	andi tmp1, 0x0F
@@ -640,21 +951,38 @@ BCD_Add:
 	pop tmp1
 	brlo msd
 lsd_correct:
+	push tmp1
+	andi tmp1, 0xF0
+	cpi tmp1, 0xA0
+	pop tmp1
+	brlo only_lsd_correct
+	rjmp correct_both
+only_lsd_correct:
 	ldi tmp, 0x06
 	add tmp1, tmp
 msd:
+	sbrc r20, 0
+	rjmp msd_correct
 	push tmp1
 	andi tmp1, 0xF0
 	cpi tmp1, 0xA0
 	pop tmp1
 	brlo bcd_end
+msd_correct:
 	ldi tmp, 0x60
+	add tmp1, tmp
+	brcc bcd_end
+	sbi GPIOR0, 0 
+	rjmp bcd_end
+correct_both:
+	ldi tmp, 0x66	; correct both
 	add tmp1, tmp
 	brcc bcd_end
 	sbi GPIOR0, 0 
 bcd_end:
 	st Z, tmp1
 
+	pop r20
 	pop tmp
 	pop tmp1	
 	ret
@@ -672,42 +1000,34 @@ Send_SPI:
 SPI_Send:
 	push tmp
 	out SPDR, tmp
-#ifndef DEBUG
 spi_wait:
 	in tmp, SPSR
 	sbrs tmp, SPIF
 	rjmp spi_wait
-#endif
 	pop tmp
 	ret	
 ;--------------------
 delay_10us:
 	push tmp
 	ldi tmp, 24
-
 delay_loop:
 	dec tmp
 	brne delay_loop
-
 	pop tmp
 	ret
 ;--------------------
 delay_100ms:
-#ifndef DEBUG
 	ldi tmp2, 16
 d100_w:
 	cpi tmp2, 0x00
 	brne d100_w
-#endif
 	ret
 ;--------------------
 delay_10ms:
-#ifndef DEBUG
 	ldi tmp2, 2
 d10_w:
 	cpi tmp2, 0x00
 	brne d10_w
-#endif
 	ret
 ;--------------------
 LCD_DrawDate:
@@ -715,6 +1035,8 @@ LCD_DrawDate:
 	push ZL
 	push tmp
 	push tmp1
+	push style
+	ldi style, 0
 
 	ldi tmp1, 0xFF
 
@@ -819,6 +1141,7 @@ drw_dat_year:
 	pop frg_color
 	pop bkg_color
 drw_dat_end:
+	pop style
 	pop tmp1
 	pop tmp
 	pop ZL
@@ -830,25 +1153,16 @@ LCD_DrawDayOfWeek:
 	LASZ day_of_week 	; Рисуем день недели
 	ld tmp, Z
 
-	clr tmp1
-	LAPZ week_day_center_coo
-	add ZL, tmp
-	adc ZH, tmp1
-	lpm tmp1, Z
-
+	ldi tmp1, 6
 	mov lcd_x, tmp1
 	ldi tmp1, 60
 	mov lcd_y, tmp1
 
-	LAPZ week_day_table
-	lsl tmp
-	add ZL, tmp
-	clr tmp
-	adc ZH, tmp
-	lpm tmp, Z+
-	lpm tmp1, Z
-	mov ZL, tmp
-	mov ZH, tmp1
+	LAPZ week_day_names
+	ldi tmp1, 12
+	mul tmp, tmp1
+	add ZL, r0
+	adc ZH, r1
 	rcall LCD_DrawStringPM
 	pop tmp1
 	ret
@@ -879,18 +1193,14 @@ month_names:
 
 week_day_names:
 mon:	.db "ПОНЕДЕЛЬНИК",0x00
-tue:	.db "ВТОРНИК",0x00
-wed:	.db "СРЕДА",0x00
-thu:	.db "ЧЕТВЕРГ",0x00
-fri:	.db "ПЯТНИЦА",0x00
-sat:	.db "СУББОТА",0x00
+tue:	.db "  ВТОРНИК  ",0x00
+wed:	.db "   СРЕДА   ",0x00
+thu:	.db "  ЧЕТВЕРГ  ",0x00
+fri:	.db "  ПЯТНИЦА  ",0x00
+sat:	.db "  СУББОТА  ",0x00
 sun:	.db "ВОСКРЕСЕНИЕ",0x00
 
-week_day_table:
-	.dw mon<<1, tue<<1, wed<<1, thu<<1, fri<<1, sat<<1, sun<<1
-
-week_day_center_coo:
-	.db 6, 22, 30, 22, 22, 22, 6, 0x00
+tim_str: .db "ТАЙМЕР",0x00,0x00
 
 corr_table:
 	.dw hour, minute, second, day, day_of_week, month, year
@@ -900,8 +1210,16 @@ corr_max_value:
 
 sie_c60_init:
 	.db	0x80,0x04,0x8A,0x54,0x45,0x52,0x43,0x02,0x0A,0x15,0x1F,0x28,0x30,0x37,0x3F,0x47,0x4C,0x54,0x65,0x75,0x80,0x85,0x00,0x03,0x05,0x07,0x09,0x0B,0x0D,0x0F,0x00,0x03,0x05,0x07,0x09,0x0B,0x0D,0x0F,0x00,0x05,0x0B,0x0F
-;	.db 7,15,
 
+w_time_corr:	; Коррекция времени. Когда часы "покажут" это время, программа "сбросит" их в 00:00:00.0 - это коррекция для "убегающих" часов
+	.db 0x00,0x00,0x00,0x00 ; доли секунд, секунды, минуты, часы
+
+
+; запись длительностей звука и пауз для сигнала
+; т.к. нет не логично запускать подряд два звука или две паузы, то каждый четный байт (с нуля) - это длительность звука (в долях секунды)
+; а каждый нечетный байт - длительность паузы
+sound_pattern:
+	.db 2,1,2,1,2,1,2,1,2,1	; пробный пи-пи-пи-пи :-)
 
 ;-------- Конец cseg
 
@@ -917,20 +1235,7 @@ sie_c60_init:
 	year: .dw 0x00
 	day_of_week: .db 0x00
 
-	tim_ovf_val: .db 0x00	; Длительность 5мс интервала (более точная корректировка)
-
-	; цифровая настройка хода (очень точная корректировка)
-	corr_period: .dd 0x00000000	; период, по прошествии которого осуществится корректировка (десятые доли секунды)
-	corr_per_cnt: .dd 0x00000000
-	corr_value: .dw 0x0000		; корректирующее значение (BCD) lsb - десятые доли секунд, msb - секунды, старший бит lsb - знак коррекции (+/-)
-
-	sec_duration: .db 0x00	; Длительность секунды (грубая корректировка)
-	sec_cnt: .db 0x00
+	timer_sec: .db 0x00	; BCD значение таймера (секунды)
+	timer_min: .db 0x00	; минуты
 
 ;-------- Конец dseg
-
-.eseg ; Сегмент eeprom
-
-sec_dur_eep: .db 0x14
-
-;-------- Конец eseg
